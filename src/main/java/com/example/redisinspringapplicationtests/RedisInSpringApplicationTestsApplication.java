4a7a996e87edaf1020db29363c69e92cbff26ae7
InKyuHwang001 <hwanginkyu42@naver.com>
2024-08-27 21:36:46 +0900

    first commit

---
+++ src/main/java/com/example/redisinspringapplicationtests/RedisInSpringApplicationTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.redisinspringapplicationtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisInSpringApplicationTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisInSpringApplicationTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/redisinspringapplicationtests/RedisInSpringApplicationTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redisinspringapplicationtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisInSpringApplicationTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
